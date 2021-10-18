659c64df22178e5e398e31b1934ba0cff15734b6
jaredhall18 <jaredhall18@gmail.com>
2021-10-18 13:42:58 -0600

    Refactored routing, added Private Route, axios with Auth, and singUp form

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -5,31 +5,49 @@ import Home from './components/Home';
 import AddEvent from './components/AddEvent';
 import EditEvent from './components/EditEvent';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import Team from './components/Team';
 import UpcomingEvents from './components/UpcomingEvents';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import Logout from './components/Logout';
+import PrivateRoute from './components/PrivateRoute';
 
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Test</p>
-      </header>
-
-    <Switch>
-      <Route />
-      <Route path='/editevent' component={EditEvent}/>
-      <Route path='/addevent' component={AddEvent}/>
-      <Route path='/team' component = {Team}/>
-      <Route path = '/upcomingevents' component = {UpcomingEvents}/>
-      <Route path='/login' component={Login}/>
-      <Route path='/signup' component={SignUp}/>
-      <Route path='/' component={Home} />
-
-    </Switch>
+      <Link to="/login">Login</Link>
+      <Link to="/">Home</Link>
+        <Header />
+        <Switch>
+          <Route path="/logout">
+            <Logout />
+          </Route>
+          <Route path="/upcomingevents">
+              <UpcomingEvents />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/team">
+            <Team />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/add">
+            <AddEvent />
+          </Route>
+          <Route path="/edit">
+            <EditEvent />
+          </Route>
+        </Switch>
+        <Footer />
     </div>
   );
 }
