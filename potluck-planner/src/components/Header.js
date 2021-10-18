e77a37eb32b42ce68d11685110f83fdae54114df
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-10-18 15:29:08 -0700

    done setting up utility and common classes

--- potluck-planner/src/components/Header.js
+++ potluck-planner/src/components/Header.js
@@ -4,10 +4,16 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     height: var(--nav-height);
     width: 100%;
     background-color: var(--secondary-color);
-    display: flex;
+
+    h3 {
+      margin-left: 4%;
+    }
   }
 `;
 
@@ -15,6 +21,7 @@ export default function Header() {
   return (
     <StyledHeader>
       <nav>
+        <h3>Potluck Planner </h3>
         <div className="nav-links-container">
           <Link />
           <Link />
