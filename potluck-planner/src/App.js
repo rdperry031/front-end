f676a52e8cc53a56ac5fa3d7a7e0e50785515a72
jaredhall18 <jaredhall18@gmail.com>
2021-10-18 11:37:42 -0600

    set up file structure and routing for project

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { Route, Link, Switch } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import AddEvent from './components/AddEvent';
+import EditEvent from './components/EditEvent';
+import Footer from './components/Footer';
+import Team from './components/Team';
+import UpcomingEvents from './components/UpcomingEvents';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Test</p>
       </header>
+
+    <Switch>
+      <Route />
+      <Route path='/editevent' component={EditEvent}/>
+      <Route path='/addevent' component={AddEvent}/>
+      <Route path='/team' component = {Team}/>
+      <Route path = '/upcomingevents' component = {UpcomingEvents}/>
+      <Route path='/login' component={Login}/>
+      <Route path='/signup' component={SignUp}/>
+      <Route path='/' component={Home} />
+
+    </Switch>
     </div>
   );
 }
